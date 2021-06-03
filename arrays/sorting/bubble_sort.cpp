d4f7840e7ecf708dbee9411b1ec85e834cc25fe7
Harsh Mathur <harshmathurx@gmail.com>
2021-06-03 17:30:45 +0530

    bubble sort and selection sort

--- arrays/sorting/bubble_sort.cpp
+++ arrays/sorting/bubble_sort.cpp
@@ -3,19 +3,22 @@ using namespace std;
 
 int main(){
 
-    int n; cin>>n;
+    int n;
+    cin>>n;
+
     int arr[n];
     for(int i=0;i<n;i++) cin>>arr[i];
 
-    //Selection sort
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i] > arr[j]){
+    int counter = 1;
+    while(counter<n){
+        for(int i=0;i<n-counter;i++){
+            if(arr[i]>arr[i+1]){
                 int temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
             }
         }
+        counter++;
     }
 
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
