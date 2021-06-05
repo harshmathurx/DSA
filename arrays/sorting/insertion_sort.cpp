ab30e4f2e30b5cdf17dd5edf1f69f163be1c82c5
Harsh Mathur <harshmathurx@gmail.com>
2021-06-05 13:54:57 +0530

    insertion sort corrected

--- arrays/sorting/insertion_sort.cpp
+++ arrays/sorting/insertion_sort.cpp
@@ -6,17 +6,16 @@ int main(){
     int arr[5] = {5,6,0,7,2};
     int n = 5;
     
-    Insertion Sort
+    //Insertion Sort
     for(int i=1;i<n;i++){
         int current = arr[i];
         int j = i-1;
-        while(arr[j] > current && j<0){
+        while(arr[j] > current && j>=0){
             arr[j+1] = arr[j];
             j--;
         }
         arr[j+1] = current;
     }
-
 
     for(int i=0;i<n;i++) cout<<arr[i]<<" ";
 
