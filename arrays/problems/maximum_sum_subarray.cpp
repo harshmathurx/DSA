35466ef92dfb86ba6e8faff90429a2ef9aa75eca
harshmathurx <harshmathurx@gmail.com>
2021-06-14 18:16:16 +0530

    maximum sum subarray - cumulative sum approach

--- arrays/problems/maximum_sum_subarray.cpp
+++ arrays/problems/maximum_sum_subarray.cpp
@@ -2,19 +2,41 @@
 #include<climits>
 using namespace std;
 
+//BruteForce
+// int maximumSumSubarray(int arr[],int n){
+//     int maxSum = INT_MIN;
+//     for(int i=0;i<n;i++){
+//         for(int j=i;j<n;j++){
+//             int sum = 0;
+//             for(int k=i;k<=j;k++){
+//                 sum += arr[k];
+//             }
+//             maxSum = max(sum,maxSum);
+//         }
+//     }
+//     return maxSum;
+// }
+
+//Cumulitive sum approach
 int maximumSumSubarray(int arr[],int n){
-    int maxSum = INT_MIN;
-    for(int i=0;i<n;i++){
-        for(int j=i;j<n;j++){
-            int sum = 0;
-            for(int k=i;k<=j;k++){
-                sum += arr[k];
-            }
+    int currSum[n+1];
+    currSum[0] = 0;
+    for(int i=1;i<=n;i++){
+        currSum[i] = currSum[i-1] + arr[i-1];
+    }
+
+    int maxSum = 0;
+    for(int i=1;i<=n;i++){
+        int sum = INT_MIN;
+        for(int j=0;j<i;i++){
+            sum = currSum[i] - currSum[j];
             maxSum = max(sum,maxSum);
         }
     }
+
     return maxSum;
 }
+
 
 int main(){
 
