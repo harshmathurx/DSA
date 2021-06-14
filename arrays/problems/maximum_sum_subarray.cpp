0761ac6e012e2a0ac3703cae3313308c8596d6ca
harshmathurx <harshmathurx@gmail.com>
2021-06-14 19:37:25 +0530

    kadane's algorithm

--- arrays/problems/maximum_sum_subarray.cpp
+++ arrays/problems/maximum_sum_subarray.cpp
@@ -17,27 +17,43 @@ using namespace std;
 //     return maxSum;
 // }
 
-//Cumulitive sum approach
-int maximumSumSubarray(int arr[],int n){
-    int currSum[n+1];
-    currSum[0] = 0;
-    for(int i=1;i<=n;i++){
-        currSum[i] = currSum[i-1] + arr[i-1];
-    }
+// //Cumulitive sum approach
+// int maximumSumSubarray(int arr[],int n){
+//     int currSum[n+1];
+//     currSum[0] = 0;
+//     for(int i=1;i<=n;i++){
+//         currSum[i] = currSum[i-1] + arr[i-1];
+//     }
 
+//     int maxSum = INT_MIN;
+
+//     for(int i=1;i<=n;i++){
+//         int sum = 0;
+//         for(int j=0;j<i;j++){   
+//             sum = currSum[i] - currSum[j];
+//             maxSum = max(sum,maxSum);
+//         }
+//     }    
+
+//     return maxSum;
+// }
+
+
+//Kadane's Algorithm
+int maximumSumSubarray(int arr[],int n){
+    int currSum = 0;
     int maxSum = INT_MIN;
 
-    for(int i=1;i<=n;i++){
-        int sum = 0;
-        for(int j=0;j<i;j++){
-            sum = currSum[i] - currSum[j];
-            maxSum = max(sum,maxSum);
+    for(int i=0;i<n;i++){
+        currSum += arr[i];
+        if(currSum<0){
+            currSum = 0;
         }
-    }    
+        maxSum = max(currSum,maxSum);
+    }
 
     return maxSum;
 }
-
 
 int main(){
 
